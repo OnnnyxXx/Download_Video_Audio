3d691a259f5c30014aeb3fc707531be4be9b1eaf
OnnnyxXx <oooivanlau2@gmail.com>
2024-03-22 19:57:37 +0300

    Update views

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 import os
+import random
+import time
 
 from aiogram.dispatcher.filters import Text
 import aiogram
@@ -22,7 +24,11 @@ async def start_commands(message: types.Message):
     start_button = ["Скачать видео и аудио"]  # "Скачать видео", 'Скачать Аудио'
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_button)
-    await message.answer('Привет! {0.first_name} 👋'.format(message.from_user), reply_markup=keyboard)
+
+    await message.answer('Ну привет {0.first_name} 👋'.format(message.from_user), reply_markup=keyboard)
+
+    photo = open("hello_user/kiss-lip-kiss.gif", 'rb')
+    await bot.send_animation(message.chat.id, photo)
 
 
 @dp.message_handler(Text(equals='Скачать видео и аудио'))
@@ -41,11 +47,23 @@ async def download_audio(message: types.Message):
     try:
         link_audio = YouTube(message.text)
         stream = link_audio.streams.filter(only_audio=True).first()
+
+        await message.answer('Долго качает, посмотри на меня')
+        time.sleep(4)
+        photo = open('photo_blat/' + random.choice(os.listdir('photo_blat')), 'rb')
+        await bot.send_photo(message.chat.id, photo)
+
         audio_path = f'Аудио/{link_audio.title}'
         stream.download(output_path='Аудио/', filename=link_audio.title)
+
+        await message.answer('Вызываю полицию на ваш адрес')
+        time.sleep(5)
+        await message.answer("Шутка....")
+
         with open(audio_path, 'rb') as audio:
             await bot.send_audio(message.chat.id, audio, caption=f'Аудио от видео: {link_audio.title}')
         await message.answer('Аудио успешно извлечено и отправлено!')
+
         os.remove(audio_path)
     except Exception as e:
         await message.answer("Произошла ошибка при извлечении аудио")
@@ -53,12 +71,21 @@ async def download_audio(message: types.Message):
     """Загрузка Видео"""
     try:
         link_video = YouTube(message.text)
+
         video_path = f'Видео/{link_video.title}'
         stream = link_video.streams.get_highest_resolution()
+        time.sleep(4)
+        photo = open('photo_blat/' + random.choice(os.listdir('photo_blat')), 'rb')
+        await bot.send_photo(message.chat.id, photo)
+
         stream.download(output_path='Видео/', filename=link_video.title)
+        await message.answer('Почти готово')
+        time.sleep(3)
+        await message.answer("Вшиваю майнер👀")
         with open(video_path, 'rb') as video:
             await bot.send_video(message.chat.id, video, caption=link_video.title)
         await message.answer(f"Видео успешно скачано!")
+
         os.remove(video_path)
     except Exception as e:
         await message.answer("Произошла ошибка при загрузке видео")
